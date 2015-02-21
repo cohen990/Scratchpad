c45d612a1bfb6e30eed1bccd937b975d1b3923c2
Samantha Cohen <samanthacohen294@gmail.com>
2015-02-21 17:48:00 +0000

    Can check price on list of products
    - introduced products
    - introduced special offers (only support 2for1)

---
+++ src/scratchpad/ConsoleApplication1/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+
+    public class StockPricer
+    {
+        private List<SpecialOffer> _specialOffers { get; set; }
+
+        public StockPricer()
+        {
+            SetSpecialOffers();
+        }
+
+        public decimal AppraiseList(params Product[] products)
+        {
+            if (!products.Any())
+            {
+                return 0;
+            }
+
+            foreach (SpecialOffer specialOffer in _specialOffers)
+            {
+                SpecialOffer offer = specialOffer;
+                specialOffer.SetCost(products.Where(x => offer.AffectedProductNames.Any(x.Name.Equals)).ToList());
+            }
+
+            return products.Sum(x => x.Cost);
+        }
+
+        private void SetSpecialOffers()
+        {
+            _specialOffers = new List<SpecialOffer>
+                {
+                    new SpecialOffer {AffectedProductNames = new List<string> {"2for1product"}}
+                };
+        }
+    }
+
+    public class Product
+    {
+        public Product(decimal price, string name)
+        {
+            Price = price;
+            Name = name;
+            Cost = price;
+        }
+
+        public decimal Price;
+
+        public decimal Cost;
+
+        public string Name;
+    }
+
+    public class SpecialOffer
+    {
+        public List<string> AffectedProductNames;
+
+        public void SetCost(params Product[] product)
+        {
+            SetCost(new List<Product>(product));
+        }
+
+        public void SetCost(List<Product> products)
+        {
+            if (products == null || !products.Any())
+                return;
+            if(!products.All(x => AffectedProductNames.Contains(x.Name)))
+                throw new ArgumentException("The products given are not in the affected products list.", "products");
+
+            for (int i = 0; i < products.Count - 1; i += 2)
+            {
+                products[i].Cost = 0;
+            }
+        }
+    }
+}
+
---
+++ src/scratchpad/ConsoleApplication1/StockPricerTests.cs
@@ -0,0 +1,204 @@
+﻿namespace ConsoleApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using NUnit.Framework;
+
+    public class StockPricerTests
+    {
+        [Test]
+        public void AppraiseList_givennolist_returns0()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList();
+
+            Assert.AreEqual(0m, result);
+        }
+
+        [Test]
+        public void AppraiseList_givenoneproduct_returnsproductprice()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(new Product(10m, "someproduct"));
+
+            Assert.AreEqual(10m, result);
+        }
+
+        [Test]
+        public void AppraiseList_giventwoproducts_returnssumofprices()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(new Product(10m, "someproduct"),
+                new Product(5m, "someotherproduct"));
+
+            Assert.AreEqual(15m, result);
+        }
+
+        [Test]
+        public void AppraiseList_giventwo2for1products_returnsdiscountedprice()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"));
+
+            Assert.AreEqual(10m, result);
+        }
+
+        [Test]
+        public void AppraiseList_givenone2for1productandoneregularproduct_returnsfullprice()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(new Product(10m, "2for1product"),
+                new Product(15m, "otherproduct"));
+
+            Assert.AreEqual(25m, result);
+        }
+
+        [Test]
+        public void AppraiseList_givenone3for1productandoneregularproduct_returnsfullprice()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"));
+
+            Assert.AreEqual(20m, result);
+        }
+
+        [Test]
+        public void AppraiseList_givenone5for1productandoneregularproduct_returnsfullprice()
+        {
+            var pricer = new StockPricer();
+
+            var result = pricer.AppraiseList(
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"),
+                new Product(10m, "2for1product"));
+
+            Assert.AreEqual(30m, result);
+        }
+    }
+
+    [TestFixture]
+    public class SpecialOfferTests
+    {
+        private SpecialOffer _offer;
+        private Product _productNotInList;
+        private Product _productInList;
+        private string _inListIdentifier;
+        private string _notInListIdentifier;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _inListIdentifier = "inlist";
+            _notInListIdentifier = "notinlist";
+            _offer = new SpecialOffer { AffectedProductNames = new List<string> { _inListIdentifier } };
+            _productNotInList = new Product(10m, _notInListIdentifier);
+            _productInList = new Product(10m, _inListIdentifier);
+        }
+
+        [Test]
+        public void SetCost_givennothing_doesntthrowexception()
+        {
+            Assert.DoesNotThrow(() => _offer.SetCost(new List<Product>()) );
+        }
+
+        [Test]
+        public void SetCost_GivenProductNotInAffectedList_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => _offer.SetCost(new List<Product> {_productNotInList}));
+        }
+
+        [Test]
+        public void SetCost_Given1Product_ChangesNothing()
+        {
+            _offer.SetCost(_productInList);
+
+            Assert.AreEqual(10m, _productInList.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given2Products_ChangesFirstProductCostToZero()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2);
+
+            Assert.AreEqual(0, inList1.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given2Products_DoesntChangeSecondProductCost()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2);
+
+            Assert.AreEqual(10m, inList2.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given4Products_ChangesFirstProductCostToZero()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+            var inList3 = new Product(10m, _inListIdentifier);
+            var inList4 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2, inList3, inList4);
+
+            Assert.AreEqual(0, inList1.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given4Products_DoesntChangeSecondProduct()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+            var inList3 = new Product(10m, _inListIdentifier);
+            var inList4 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2, inList3, inList4);
+
+            Assert.AreEqual(10m, inList2.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given4Products_ChangesThirdProductCostToZero()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+            var inList3 = new Product(10m, _inListIdentifier);
+            var inList4 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2, inList3, inList4);
+
+            Assert.AreEqual(0, inList3.Cost);
+        }
+
+        [Test]
+        public void SetCost_Given4Products_DoesntChangeFourthProductCost()
+        {
+            var inList1 = new Product(10m, _inListIdentifier);
+            var inList2 = new Product(10m, _inListIdentifier);
+            var inList3 = new Product(10m, _inListIdentifier);
+            var inList4 = new Product(10m, _inListIdentifier);
+
+            _offer.SetCost(inList1, inList2, inList3, inList4);
+
+            Assert.AreEqual(10m, inList4.Cost);
+        }
+    }
+}
